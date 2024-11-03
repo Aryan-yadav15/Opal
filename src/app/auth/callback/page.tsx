208a7eeed7f7e7ad2393b6b229aafe2bfc72a24f
Aryan-yadav15 <a2003yadav@gmail.com>
2024-11-03 13:00:39 +0530

    log fix

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,19 +1,19 @@
-import { onAuthenticateUser } from '@/actions/user';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const authCallbackPage = async (props: Props) => {
   const auth = await onAuthenticateUser();
- 
+
   if (auth.status === 200 || auth.status === 201) {
     return redirect(`/dashboard/${auth.user?.firstname}${auth.user?.lastname}`);
   }
   if (auth.status === 500 || auth.status === 400 || auth.status === 404) {
     return redirect("/auth/sign-in");
+    console.log("adsasd");
   }
+};
 
-}
-
-export default authCallbackPage
+export default authCallbackPage;
